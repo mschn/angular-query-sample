dd987a86cffd42e68d79a8b439a7ba099a9a0d38
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2024-05-30 22:55:30 +0200

    feat: improve home

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
